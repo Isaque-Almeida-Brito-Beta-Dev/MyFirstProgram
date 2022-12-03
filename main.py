1ac235136c1560f3107494a7704187909e4b9f63
Isaque-Almeida-Brito <zack.almeida.brito@gmail.com>
2022-12-03 07:25:00 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,5 +4,16 @@ from time import sleep
 print ("Olá, Mundo")
 sleep(1)
 nome = input("Quem é Você")
-idade = input ("Quantos anos Você Tem?")
-print ("Certo você tem " +idade)
+idade = input ("Quantos anos Você Tem?") 
+print ("Seu Nome é " + nome + " E Você Tem " + idade + " Anos Certo?")
+sleep(1)
+
+m1 = int (input ("Digite algum numero " ))
+m2 = int (input ("Digite o segundo numero "))
+print (m1 + m2 )
+
+#continuas hehe
+
+
+
+
